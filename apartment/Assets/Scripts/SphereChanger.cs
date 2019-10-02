ed19134c8ede7e402e8407bd9d4914b575329960
lchoo <lchoo1996@gmail.com>
2019-10-01 20:58:51 -0700

    built: webgl

--- apartment/Assets/Scripts/SphereChanger.cs
+++ apartment/Assets/Scripts/SphereChanger.cs
@@ -6,7 +6,7 @@ public class SphereChanger : MonoBehaviour {
     public GameObject m_Fader;
 
     //This ensures that we don't mash to change spheres
-    bool changing = false;
+    public bool changing = false;
 
 
     void Awake()
@@ -34,46 +34,56 @@ public class SphereChanger : MonoBehaviour {
         //Ensure we have a fader object
         if (m_Fader != null)
         {
+            Debug.Log("Got Fader!");
             //Fade the Quad object in and wait 0.75 seconds
+
             StartCoroutine(FadeIn(0.75f, m_Fader.GetComponent<Renderer>().material));
+           
             yield return new WaitForSeconds(0.75f);
 
             //Change the camera position
             Camera.main.transform.parent.position = nextSphere.position;
 
+           
             //Fade the Quad object out 
             StartCoroutine(FadeOut(0.75f, m_Fader.GetComponent<Renderer>().material));
+           
             yield return new WaitForSeconds(0.75f);
         }
         else
         {
+            Debug.Log("No Fader!");
             //No fader, so just swap the camera position
-            Camera.main.transform.parent.position = nextSphere.position;
+            Camera.main.transform.parent.position = nextSphere.position;           
         }
 
 
     }
-
+    
 
     IEnumerator FadeOut(float time, Material mat)
     {
-        //While we are still visible, remove some of the alpha colour
-        while (mat.color.a > 0.0f)
-        {
-            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, mat.color.a - (Time.deltaTime / time));
-            yield return null;
-        }
+           
+            //While we are still visible, remove some of the alpha colour
+            while (mat.color.a >= 0.0f)
+            {
+                mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, mat.color.a - (Time.deltaTime / time));               
+                //change the bool to true when its completely fade out                      
+                yield return null;
+        }    
     }
 
 
     IEnumerator FadeIn(float time, Material mat)
     {
-        //While we aren't fully visible, add some of the alpha colour
-        while (mat.color.a < 1.0f)
-        {
-            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, mat.color.a + (Time.deltaTime / time));
-            yield return null;
-        }
+            //While we aren't fully visible, add some of the alpha colour
+            while (mat.color.a <= 1.0f)
+            {                
+                mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, mat.color.a + (Time.deltaTime / time));               
+                //change the bool to true when its completely fade out              
+                yield return null; 
+            }
+             
     }
 
 
