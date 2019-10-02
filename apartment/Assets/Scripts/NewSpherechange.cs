22c0a6f5e66978ed4b61f5bd27b1a97cb9ed73ba
lchoo <lchoo1996@gmail.com>
2019-10-02 00:47:42 -0700

    refactor: added animation

--- apartment/Assets/Scripts/NewSpherechange.cs
+++ apartment/Assets/Scripts/NewSpherechange.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SphereChanger : MonoBehaviour {
+public class NewSpherechange : MonoBehaviour
+{
     public GameObject m_Fader;
 
     //This ensures that we don't mash to change spheres
-    public bool changing = false;
-
+    public bool changing = false;    
 
     void Awake()
-    {       
+    {
 
         //Check if we found something
         if (m_Fader == null)
@@ -21,9 +21,17 @@ public class SphereChanger : MonoBehaviour {
 
     public void ChangeSphere(Transform nextSphere)
     {
-
+        if(m_Fader == null)
+        {
+           //no fader, so just change the position of camera
+            Camera.main.transform.parent.position = nextSphere.position;
+        }
+        else
+        {
+            //triger the animation
+        }
         //Start the fading process
-        StartCoroutine(FadeCamera(nextSphere));
+        //StartCoroutine(FadeCamera(nextSphere));
 
     }
 
@@ -43,52 +51,51 @@ public class SphereChanger : MonoBehaviour {
             //Fade the Quad object in and wait 0.75 seconds
 
             StartCoroutine(FadeIn(0.75f, m_Fader.GetComponent<Renderer>().material));
-           
+
             yield return new WaitForSeconds(0.75f);
 
-            //Change the camera position
-            Camera.main.transform.parent.position = nextSphere.position;
+          
 
-           
+
             //Fade the Quad object out 
             StartCoroutine(FadeOut(0.75f, m_Fader.GetComponent<Renderer>().material));
-           
+
             yield return new WaitForSeconds(0.75f);
         }
         else
         {
             Debug.Log("No Fader!");
             //No fader, so just swap the camera position
-            Camera.main.transform.parent.position = nextSphere.position;           
+            Camera.main.transform.parent.position = nextSphere.position;
         }
 
 
     }
-    
+
 
     IEnumerator FadeOut(float time, Material mat)
     {
-           
-            //While we are still visible, remove some of the alpha colour
-            while (mat.color.a >= 0.0f)
-            {
-                mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, mat.color.a - (Time.deltaTime / time));               
-                //change the bool to true when its completely fade out                      
-                yield return null;
-        }    
+
+        //While we are still visible, remove some of the alpha colour
+        while (mat.color.a >= 0.0f)
+        {
+            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, mat.color.a - (Time.deltaTime / time));
+            //change the bool to true when its completely fade out                      
+            yield return null;
+        }
     }
 
 
     IEnumerator FadeIn(float time, Material mat)
     {
-            //While we aren't fully visible, add some of the alpha colour
-            while (mat.color.a <= 1.0f)
-            {                
-                mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, mat.color.a + (Time.deltaTime / time));               
-                //change the bool to true when its completely fade out              
-                yield return null; 
-            }
-             
+        //While we aren't fully visible, add some of the alpha colour
+        while (mat.color.a <= 1.0f)
+        {
+            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, mat.color.a + (Time.deltaTime / time));
+            //change the bool to true when its completely fade out              
+            yield return null;
+        }
+
     }
 
 
